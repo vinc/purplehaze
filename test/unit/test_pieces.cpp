439ce9665e8eea9465bfb77f7034eff2de0628e3
Vincent Ollivier <contact@vincentollivier.com>
2012-03-18 17:51:09 +0100

    Add test case for detecting pawn ending

--- test/unit/test_pieces.cpp
+++ test/unit/test_pieces.cpp
@@ -1,4 +1,5 @@
 #include "../../src/pieces.h"
+#include "../../src/game.h"
 #include "gtest/gtest.h"
 
 TEST(PiecesTest, Constructor)
@@ -47,24 +48,41 @@ TEST(PiecesTest, Count)
 {
     Pieces pieces;
     for (const Color &c : COLORS) {
-        EXPECT_EQ(0, pieces.count(c));
         for (const PieceType &t : PIECE_TYPES) {
+            EXPECT_EQ(0, pieces.count(c));
             EXPECT_EQ(0, pieces.count(c, t));
             for (int i = 0; i < 9; ++i) {
+                EXPECT_EQ(i, pieces.count(c));
                 EXPECT_EQ(i, pieces.count(c, t));
                 pieces.inc_nb_pieces(c, t);
+                EXPECT_EQ(i + 1, pieces.count(c));
                 EXPECT_EQ(i + 1, pieces.count(c, t));
             }
             for (int i = 9; i > 0; --i) {
+                EXPECT_EQ(i, pieces.count(c));
                 EXPECT_EQ(i, pieces.count(c, t));
                 pieces.dec_nb_pieces(c, t);
+                EXPECT_EQ(i - 1, pieces.count(c));
                 EXPECT_EQ(i - 1, pieces.count(c, t));
             }
+            EXPECT_EQ(0, pieces.count(c));
             EXPECT_EQ(0, pieces.count(c, t));
         }
-        EXPECT_EQ(0, pieces.count(c));
     }
 }
+
+TEST(PiecesTest, CountBoard)
+{
+    Game game;
+    game.init("8/8/1k6/ppp4P/6P1/5P2/4P3/4K3 w - - 0 1");
+    EXPECT_EQ(5, game.pieces.count(WHITE));
+    EXPECT_EQ(4, game.pieces.count(WHITE, PAWN));
+    EXPECT_EQ(1, game.pieces.count(WHITE, KING));
+    EXPECT_EQ(4, game.pieces.count(BLACK));
+    EXPECT_EQ(3, game.pieces.count(BLACK, PAWN));
+    EXPECT_EQ(1, game.pieces.count(BLACK, KING));
+}
+
 
 TEST(PiecesTest, Size)
 {
