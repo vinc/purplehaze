610d79bc47d8a0b9d0e80f1a1c58903f951db488
Vincent Ollivier <contact@vincentollivier.com>
2012-07-01 15:27:28 +0200

    Update copyright

--- src/board.h
+++ src/board.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2007-2011 Vincent Ollivier
+/* Copyright (C) 2007-2012 Vincent Ollivier
  *
  * Purple Haze is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
